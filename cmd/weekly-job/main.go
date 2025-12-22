01634388d65ce3a6efe368a3b882557dea4d948a
annieslip1 <anniejavakhishvili@AnnieJavakhishvilis-Laptop.local>
2025-12-22 12:11:17 +0400

    write job for sending notifications

--- cmd/weekly-job/main.go
+++ cmd/weekly-job/main.go
@@ -3,32 +3,85 @@ package main
 import (
 	"context"
 	"log"
-	"time"
+
+	"github.com/ani-javakhishvili/apartments-platform/domain/models"
+	esStorage "github.com/ani-javakhishvili/apartments-platform/domain/storage/elasticsearch"
+	elasticsearch "github.com/elastic/go-elasticsearch/v8"
+
+	"github.com/ani-javakhishvili/apartments-platform/domain/storage/postgres"
+
+	apartmentRepo "github.com/ani-javakhishvili/apartments-platform/domain/apartment"
 )
+
+type WeeklyJob struct {
+	apartmentRepo apartmentRepo.Repository
+	esRepo        EsRepository
+}
+
+type EsRepository interface {
+	PercolateApartment(ctx context.Context, a models.Apartment) ([]string, error)
+}
+
+func NewWeeklyJob(
+	apartmentRepo apartmentRepo.Repository,
+	esRepo EsRepository,
+
+) *WeeklyJob {
+	return &WeeklyJob{
+		apartmentRepo: apartmentRepo,
+		esRepo:        esRepo,
+	}
+}
 
 func main() {
 	ctx := context.Background()
+	esClient, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{"http://elasticsearch:9200"},
+	})
+	if err != nil {
+		log.Fatalf("elasticsearch client init failed: %v", err)
+	}
 
-	log.Println("Weekly job started")
+	aptRepo := postgres.NewApartmentPostgresRepo()
+	esRepo := esStorage.NewEsRepo(esClient)
 
-	if err := runWeeklyJob(ctx); err != nil {
+	job := NewWeeklyJob(aptRepo, esRepo)
+
+	if err := job.Run(ctx); err != nil {
 		log.Fatalf("Weekly job failed: %v", err)
 	}
 
-	log.Println("Weekly job finished")
 }
 
-func sendNotification(userID int, apartmentIDs []int) {
-	// here goes email sending logic -  with email, push, etc.
-	log.Printf("Sending notification to user %d: matched apartments %v\n", userID, apartmentIDs)
-}
+func (j *WeeklyJob) Run(ctx context.Context) error {
 
-func runWeeklyJob(ctx context.Context) error {
+	apartments, err := j.apartmentRepo.GetAll(ctx)
+	if err != nil {
+		return err
+	}
+
+	userMatches := map[string][]int{} // userID → apartmentIDs
+
+	for _, a := range apartments {
+		users, err := j.esRepo.PercolateApartment(ctx, a)
+		if err != nil {
+			log.Printf("percolation failed for apartment %d: %v", a.ID, err)
+			continue
+		}
+
+		for _, userID := range users {
+			userMatches[userID] = append(userMatches[userID], a.ID)
+		}
+	}
+
+	for userID, apartmentIDs := range userMatches {
+		sendNotification(userID, apartmentIDs)
+	}
 
 	return nil
 }
 
-func getCurrentWeek() int {
-	_, week := time.Now().ISOWeek()
-	return week
+func sendNotification(userID string, apartmentIDs []int) {
+	// here goes email sending logic -  with email, push, etc.
+	log.Printf("Sending notification to user %d: matched apartments %v\n", userID, apartmentIDs)
 }
